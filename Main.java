04050938bde8628abb948e0f1cb46a122613d0b1
gnouh404 <090297huong@gmail.com>
2024-09-30 09:50:29 +0700

    merge conflict add Huong

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Changa");
+        System.out.println("Hello Chanan");
+        System.out.println("Hello Changa, Huong");
     }
 }
